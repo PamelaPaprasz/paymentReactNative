e5e130f670a8982942e36fd7b5974dc40572f55d
Pamela Paprasz <papraszpamela@gmail.com>
2018-04-12 11:33:44 +0200

    add hello text and colors to the project

--- PaymentApp/App.js
+++ PaymentApp/App.js
@@ -5,9 +5,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.texts}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.texts}>Changes you make will automatically reload.</Text>
+        <Text style={styles.texts}>Shake your phone to open the developer menu.</Text>
+        <Text style={{color: '#6b8af8'}}>Haho Ferik and Bajuszok!</Text>
       </View>
     );
   }
@@ -16,8 +17,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CDCDCD',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texts: {
+    color: '#f96b30',
   },
 });
